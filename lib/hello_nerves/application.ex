e2279a9b6a9344a214f5406e1992f9c68a56dd31
pojiro <pojiro.jp@gmail.com>
2019-11-26 14:27:36 +0900

    add a feature to start measurement after boot

--- lib/hello_nerves/application.ex
+++ lib/hello_nerves/application.ex
@@ -33,6 +33,7 @@ defmodule HelloNerves.Application do
       # Children for all targets except host
       # Starts a worker by calling: HelloNerves.Worker.start_link(arg)
       # {HelloNerves.Worker, arg},
+      {HelloNerves.Co2, 0}
     ]
   end
 
--- lib/hello_nerves/co2.ex
+++ lib/hello_nerves/co2.ex
@@ -1,7 +1,27 @@
 defmodule HelloNerves.Co2 do
+  use GenServer
   @address 0x31
 
+  def start_link(ppm) do
+    GenServer.start_link(__MODULE__, ppm, name: __MODULE__)
+    get()
+  end
+
+  def offset(x) do
+    GenServer.call(__MODULE__, {:offset, x})
+  end
+
   def get() do
+    IO.puts(inspect(GenServer.call(__MODULE__, :get)) <> " ppm")
+    :timer.sleep(5000)
+    get()
+  end
+
+  def prev() do
+    GenServer.call(__MODULE__, :prev)
+  end
+
+  def _get() do
     alias Circuits.I2C
 
     {:ok, ref} = I2C.open("i2c-2")
@@ -17,12 +37,25 @@ defmodule HelloNerves.Co2 do
           x*256+y
         {:ok, <<255,_,_,_,_,_,_>>=data} ->
           IO.inspect(data)
-        {:error, :i2c_nak} ->
-          "i2c_nak"
+        {:error, term} ->
+          IO.inspect(term)
       end
 
     I2C.close(ref)
 
     result
+  end
+
+  def handle_call(:prev, _from, ppm) do
+    {:reply, ppm, ppm}
+  end
+
+  def handle_call(:get, _from, _) do
+    ppm = _get()
+    {:reply, ppm, ppm}
+  end
+
+  def handle_call({:offset, offset}, _from, ppm) do
+    {:reply, ppm + offset, ppm + offset}
   end
 end
